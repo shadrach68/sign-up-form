7237c576f22e4b2e03c5e3e12f685dc9bfd76492
shadrach68 <ugwokeshadrach68@gmail.com>
2025-09-02 19:31:31 +0100

    "added credential function"

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ button.addEventListener("click", hover);
 function hover(){
     button.style.boxShadow = "0 0 10px blue";
 }
+
+function credentials(){
+    const email = document.getElementById("inpemail").value;
+    const password = document.getElementById("inppass").value;
+    const input = document.getElementById("input").value;
+
+    console.log("The username entered: ", input);
+    console.log("The password entered: ", password);
+    console.log("The email entered: ", email);
+}
